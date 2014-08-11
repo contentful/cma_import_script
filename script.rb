b340a30058e8b175b68d4ca811abda3db92705d8
Tomasz Warkocki <warkocz@codequest.com>
2014-08-11 09:30:16 +0200

    small refactor

--- script.rb
+++ script.rb
@@ -123,11 +123,6 @@ beers_entries.each do |_id, beer|
   beer.unpublish if beer.abv < 1
 end
 
-puts 'unpublish all Beer entries where ABV is lower then 1'
-beers_entries.each do |_id, beer|
-  beer.unpublish if beer.abv < 1
-end
-
 puts 'destroy all Beer entries where ABV is lower then 1'
 beers_entries.each do |_id, beer|
   beer.destroy if beer.abv < 1
